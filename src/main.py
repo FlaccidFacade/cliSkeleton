aedb35b2a31fb6f13b51bab7cc76fdaaee325259
FlaccideFacade <zachary.glenn.turner@gmail.com>
2022-01-05 03:13:36 -0500

    adjust for prompt

--- src/main.py
+++ src/main.py
@@ -2,17 +2,25 @@
 
 
 import click
-import commands.scrambler as scrambler
 
+# import commands.scrambler as scrambler
 
+#TODO REMOVE THIS STUFF BEFORE SUBMITION FOR CLEAN UP
 
-@click.group(help="simple cli tool example")
-def start():
-  """used as an entry point for the cli program
-  """
-  pass
+# @click.group(help="simple cli tool example")
+# def start():
+#   """used as an entry point for the cli program
+#   """
+#   pass
   
-start.add_command(scrambler.scrambler)
+# start.add_command(scrambler.scrambler)
+
+
+@click.command()
+@click.argument('files', nargs=-1, type=click.Path())
+def start(files):
+  for filename in files:
+    click.echo(filename)
 
 if __name__ == '__main__':
   start()
